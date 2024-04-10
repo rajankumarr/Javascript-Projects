2eaecdd1e2f7eae8abe6d41701b092e960895891
rajankumarr <84615798+rajankumarr@users.noreply.github.com>
2024-04-10 22:55:32 +0530

    Add files via upload

---
+++ Light&Dark Mode/script.js
@@ -0,0 +1,19 @@
+let changeMode=document.querySelector("#mode");
+let currMode="light";
+let body=document.querySelector("body");
+changeMode.addEventListener("click", ()=>{
+    if(currMode==="light"){
+        currMode="dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }
+    else{
+        currMode="light"
+        body.classList.add("light");
+        body.classList.remove("dark");
+
+
+    }
+
+    console.log(currMode);
+})
